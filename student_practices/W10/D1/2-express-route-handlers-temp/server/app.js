ac9c50206bb0a92b1eef72caf31ddd66316efd89
cpeter41 <chris.steven.peters@gmail.com>
2024-02-05 15:10:14 -0800

    02practice

--- student_practices/W10/D1/2-express-route-handlers-temp/server/app.js
+++ student_practices/W10/D1/2-express-route-handlers-temp/server/app.js
@@ -25,6 +25,20 @@ const express = require('express');
 const app = express();
 
 // Your code here 
+app.use(express.json());
+
+app.use((req, res, next) => {
+    console.log("Request Body:", req.body);
+    next();
+})
+
+app.get("/artists", (req, res) => res.json(getAllArtists()));
+
+app.post("/artists", (req, res) => {
+    const artist = addArtist(req.body);
+    res.status(201);
+    res.json(artist);
+})
 
 // DO NOT MODIFY
 if (require.main === module) {
