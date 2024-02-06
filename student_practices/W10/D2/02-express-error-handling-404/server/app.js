99137d92003d5ac1da0a1594732e4d7bc0b5078f
cpeter41 <chris.steven.peters@gmail.com>
2024-02-06 13:03:16 -0800

    d2 init

--- student_practices/W10/D2/02-express-error-handling-404/server/app.js
+++ student_practices/W10/D2/02-express-error-handling-404/server/app.js
@@ -5,5 +5,20 @@ app.get('/', (req, res) => {
   res.send('GET / This is the root URL');
 });
 
+app.use((req, res, next) => {
+    const error = new Error("Sorry, the requested resource couldn't be found");
+    error.statusCode = 404;
+    next(error);
+});
+
+app.use((err, req, res, next) => {
+    const status = err.statusCode || 500;
+    res.json({
+        message: err.message,
+        statusCode: status
+    });
+    next();
+});
+
 const port = 5000;
 app.listen(port, () => console.log('Server is listening on port', port));
