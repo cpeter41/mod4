d098a74e489a191cd2bb51e0ef9ca5ee1cb58629
cpeter41 <chris.steven.peters@gmail.com>
2024-02-26 08:06:59 -0800

    W12PA done

--- student_practices/W12/aa12-assessment-practice-sequelize-search-filter-pagination/routes/players.js
+++ student_practices/W12/aa12-assessment-practice-sequelize-search-filter-pagination/routes/players.js
@@ -1,42 +1,57 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const { Player } = require('../db/models');
+const { Player } = require("../db/models");
 
-router.get('/', async (req, res) => {
-  let { firstName, number } = req.query;
+router.get("/", async (req, res) => {
+    let { firstName, number, page, size } = req.query;
+    page = parseInt(page);
+    size = parseInt(size);
 
-  const where = {};
-  if (firstName && firstName !== '') {
-    where.firstName = firstName[0].toLowerCase() + firstName.slice(1);
-  } else if (firstName === '') {
-    res.status(400);
-    return res.json({
-      errors: [
-        { message: 'firstName filter should not be empty' }
-      ]
-    });
-  }
+    if (isNaN(page) || page < 0) page = 1;
+    if (isNaN(size) || size < 0) size = 2;
+    if (size > 10) size = 10;
 
-  if (number) {
-    if (!isNaN(number) && number <= 0) {
-      where.number = parseInt(number);
-    } else {
-      res.status(400);
-      return res.json({
-        errors: [
-          { message: 'number filter should be a number greater or equal to 0' }
-        ]
-      });
+    const pagination = {
+        limit: size,
+        offset: size * (page - 1),
+    };
+
+    const where = {};
+    if (firstName && firstName !== "") {
+        where.firstName = firstName[0].toUpperCase() + firstName.slice(1);
+    } else if (firstName === "") {
+        res.status(400);
+        return res.json({
+            errors: [{ message: "firstName filter should not be empty" }],
+        });
     }
-  }
 
-  const players = await Player.findAll({
-    where,
-  });
-  return res.json({
-    players,
-  });
+    if (number) {
+        if (!isNaN(number) && number >= 0) {
+            where.number = parseInt(number);
+        } else {
+            res.status(400);
+            return res.json({
+                errors: [
+                    {
+                        message:
+                            "number filter should be a number greater or equal to 0",
+                    },
+                ],
+            });
+        }
+    }
+
+    const players = await Player.findAll({
+        where,
+        ...pagination,
+    });
+    return res.json({
+        players,
+        page,
+        size
+    });
 });
 
 module.exports = router;
